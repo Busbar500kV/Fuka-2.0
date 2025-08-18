7d3b89fb237057db8d018f0b66667e1e9ce328a4
Busbar500kV <yasasponweera@gmail.com>
2025-08-17 19:41:04 -0700

    Create organism.py

---
+++ core/organism.py
@@ -0,0 +1,9 @@
+from dataclasses import dataclass, field
+from typing import List
+
+@dataclass
+class History:
+    t: List[int] = field(default_factory=list)
+    E_cell: List[float] = field(default_factory=list)
+    E_env:  List[float] = field(default_factory=list)
+    E_flux: List[float] = field(default_factory=list)
